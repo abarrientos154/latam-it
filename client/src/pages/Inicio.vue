7f27d23ea53e8d408556ead5e27e126e53a5e934
reile <reilefer17@gmail.com>
2021-07-04 17:09:46 -0400

    inicio

--- client/src/pages/Inicio.vue
+++ client/src/pages/Inicio.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="full-window column" @click="$router.push('/home')">
     <div class="absolute-center full-width">
-      <q-img src="latam-color.png" class="full-height">
+      <q-img src="EJEMPLO-RESEÑA-LIDERAZGO.jpg" class="full-height">
       </q-img>
     </div>
   </div>
