99452d807ddae11b77f1c5b131da0835ae5a53dd
reile <reilefer17@gmail.com>
2021-07-05 13:19:03 -0400

    actividades ajuste de proporcion

--- client/src/pages/Actividad.vue
+++ client/src/pages/Actividad.vue
@@ -20,97 +20,96 @@
           </q-card>
         </div>
         <q-carousel class="col" v-model="slide" infinite>
-          <q-carousel-slide v-for="(hoja, index) in hojas" :key="index" :name="index + 1">
+          <q-carousel-slide v-for="(hoja, index) in hojas" :key="index" :name="index + 1" class="q-px-none">
             <div class="row full-height">
               <div class="col column justify-center" style="min-width: 300px;">
-                <div class="row items-center full-width q-mb-lg">
-                  <div class="text-subtitle1 text-white full-width text-bold bg-primary q-pa-md" style="border-radius: 10px;">{{hoja.pregunta}}</div>
+                <div class="row items-center full-width q-mb-md">
+                  <div class="text-subtitle1 text-white full-width text-bold bg-primary q-pa-sm" style="border-radius: 10px;">{{hoja.pregunta}}</div>
                 </div>
                 <div class="row items-center full-width">
-                  <div class="text-h5 text-white full-width text-bold bg-primary q-pa-md" style="border-radius: 10px;">
-                    <q-scroll-area style="height: 250px">
+                  <div class="text-subtitle1 text-white full-width text-bold bg-primary q-pa-sm" style="border-radius: 10px;">
+                    <q-scroll-area style="height: 150px">
                       {{hoja.descriccion}}
                     </q-scroll-area>
                   </div>
                 </div>
               </div>
-              <div class="col-6 q-pl-lg column items-center">
+              <div class="col q-pl-lg column items-center">
                 <div class="col row items-center full-width" v-for="element in hoja.respuestas" :key="element.id">
-                  <q-radio size="50px" :color="element.id === hoja.correcto &&  submit ? 'positive' : submit ? 'negative' : 'primary'" class="text-h5 text-primary text-bold full-width items-center" v-model="res" :val="element" :label="element.name"/>
+                  <q-radio size="50px" :color="element.id === hoja.correcto &&  submit ? 'positive' : submit ? 'negative' : 'primary'" class="text-subtitle1 text-primary text-bold full-width items-center" v-model="res" :val="element" :label="element.name"/>
                 </div>
               </div>
             </div>
           </q-carousel-slide>
         </q-carousel>
-        <div class="col2 q-pb-md q-px-md column items-center justify-center">
-          <q-btn class="q-pa-sm text-h5 text-bold" color="primary" label="Validar" @click="validarRes()" no-caps style="width: 250px; border-radius: 10px;"/>
+        <div class="col2 column items-center justify-center">
+          <q-btn class="q-pa-xs text-subtitle1 text-bold" color="primary" label="Validar" @click="validarRes()" no-caps style="width: 250px; border-radius: 10px;"/>
         </div>
       </div>
 
-      <div v-else-if="actividad[0].id === 4" class="window-height column">
-        <div class="column items-center q-pt-md q-px-md">
-          <q-card style="border-radius: 24px; width: 100%; height: 200px;">
+      <div v-else-if="actividad[0].id === 4" class="window-height column q-pa-md">
+        <div class="column items-center">
+          <q-card style="border-radius: 15px; width: 100%; height: 150px;">
             <q-img :src="actividad[0].src" class="full-height">
               <div class="absolute-full row">
-                <div>
-                  <div class="text-h2 text-bold q-pa-md">{{modulo[0].name}}</div>
-                  <div class="col q-pl-md">
-                    <q-img src="latam-color.png" style="width: 225px"/>
+                <div class="q-pa-xs">
+                  <div class="text-h3 text-bold q-mb-sm">{{modulo[0].name}}</div>
+                  <div class="col">
+                    <q-img src="esquinas.png" style="width: 50px"/>
                   </div>
                 </div>
-                <div class="column items-end absolute-bottom q-pa-lg">
-                  <div class="text-h3 text-bold">{{actividad[0].titulo}}</div>
-                  <div class="text-h5 text-right" style="width: 700px;">{{actividad[0].hojas[slide - 1].descriccion}}</div>
+                <div class="column items-end absolute-bottom q-pa-md">
+                  <div class="text-h4 text-bold">{{actividad[0].titulo}}</div>
                 </div>
               </div>
             </q-img>
           </q-card>
         </div>
         <q-carousel class="col" v-model="slide" infinite>
-          <q-carousel-slide v-for="(hoja, index) in hojas" :key="index" :name="index + 1">
+          <q-carousel-slide v-for="(hoja, index) in hojas" :key="index" :name="index + 1" class="q-px-none">
             <div class="row full-height">
               <div style="min-width: 300px;">
                 <draggable class="column items-center" style="height: 100%;" v-model="hoja.preguntas" group="people" @input="selecPre(hoja.preguntas)">
                   <div class="col row items-center full-width" v-for="(element, index) in hoja.preguntas" :key="index" style="max-width: 400px;">
-                    <div class="text-h5 text-white full-width text-bold bg-primary q-pa-md" style="border-radius: 10px;">{{element.name}}</div>
+                    <div class="text-subtitle1 text-white full-width text-bold bg-primary q-pa-sm" style="border-radius: 10px;">{{element.name}}</div>
                   </div>
                 </draggable>
               </div>
               <div class="col q-pl-lg column items-center">
                 <div class="col row items-center full-width" v-for="element in hoja.respuestas" :key="element.id">
-                  <div :class="val[element.id - 1] ? 'text-h5 text-white text-bold full-width items-center bg-positive q-pa-md' : val[element.id - 1] === false ? 'text-h5 text-white text-bold full-width items-center bg-negative q-pa-md' : 'text-h5 text-white text-bold full-width items-center bg-primary q-pa-md'" style="border-radius: 10px;">{{element.name}}</div>
+                  <div :class="val[element.id - 1] ? 'text-subtitle1 text-white text-bold full-width items-center bg-positive q-pa-sm' : val[element.id - 1] === false ? 'text-subtitle1 text-white text-bold full-width items-center bg-negative q-pa-sm' : 'text-subtitle1 text-white text-bold full-width items-center bg-primary q-pa-sm'" style="border-radius: 10px;">{{element.name}}</div>
                 </div>
               </div>
             </div>
           </q-carousel-slide>
         </q-carousel>
-        <div class="col2 q-pb-md q-px-md column items-center justify-center">
-          <q-btn class="q-pa-sm text-h5 text-bold" color="primary" label="Validar" @click="validarRes()" no-caps style="width: 250px; border-radius: 10px;"/>
+        <div class="col2 column items-center justify-center">
+          <q-btn class="q-pa-xs text-subtitle1 text-bold" color="primary" label="Validar" @click="validarRes()" no-caps style="width: 250px; border-radius: 10px;"/>
         </div>
       </div>
       <q-dialog v-model="verVal" persistent>
-        <q-card v-if="error" style="border-radius: 24px;" class="column">
-          <div class="col-2 row items-center justify-center text-primary text-h4 text-bold q-pa-lg">Vuelve a intentarlo</div>
-          <div class="column items-center justify-center q-pt-lg">
-            <q-img src="tryagain.png" style="width: 200px"/>
+        <q-card v-if="error" style="border-radius: 24px;" class="column q-pa-md">
+          <div class="col-2 row items-center justify-center text-primary text-h6 text-bold q-mb-sm">Vuelve a intentarlo</div>
+          <div class="column items-center justify-center q-mb-sm">
+            <q-img src="tryagain.png" style="width: 150px"/>
           </div>
-          <div class="column items-center q-pa-lg">
-            <div class="text-h6 text-no-wrap">Recuerda que tienes que tener todas las alternativas</div>
+          <div class="column items-center">
+            <div class="text-subtitle1 text-no-wrap">Recuerda que tienes que tener todas las alternativas</div>
             <div class="row">
-              <div class="text-h6 text-primary q-pr-xs text-bold">correctas</div>
-              <div class="text-h6">{{slide === hojas.length ? 'para poder completar el examen' : 'para pasar a las sigientes preguntas.'}}</div>
+              <div class="text-subtitle1 text-primary q-pr-xs text-bold">correctas</div>
+              <div class="text-subtitle1">{{slide === hojas.length ? 'para poder completar el examen' : 'para pasar a las sigientes preguntas.'}}</div>
             </div>
           </div>
-          <q-card-actions class="col2 q-pb-lg" align="center">
-            <q-btn class="q-pa-sm text-h6" color="primary" label="Reintentar" @click="reintentar()" no-caps style="width: 50%; border-radius: 10px;"/>
+          <q-card-actions align="center">
+            <q-btn class="q-pa-xs text-subtitle1 text-bold" color="primary" label="Reintentar" @click="reintentar()" no-caps style="width: 50%; border-radius: 10px;"/>
           </q-card-actions>
         </q-card>
-        <q-card v-else style="border-radius: 24px;">
-          <div class="text-center text-primary text-h4 text-bold q-pa-lg">Respondiste con exito</div>
-          <div class="column items-center justify-center q-pt-lg">
-            <q-img src="completo.png" style="width: 400px"/>
+        <q-card v-else style="border-radius: 24px;" class="q-pa-md">
+          <div class="text-center text-primary text-h6 text-bold q-mb-sm">Respondiste con exito</div>
+          <div class="column items-center justify-center q-mb-sm">
+            <q-img src="completo.png" style="width: 300px"/>
           </div>
-          <div class="text-center text-h6 q-px-lg q-pt-lg q-pb-xl">{{slide === hojas.length ? 'Felicidades haz completado el examen con éxito' : 'Te estamos redirecionando a la siguientes preguntas.'}}</div>
+          <div class="text-center text-subtitle1 text-bold">{{slide === hojas.length ? 'Felicidades haz completado el examen con éxito' : 'Te estamos redirecionando a la siguientes preguntas.'}}</div>
         </q-card>
       </q-dialog>
     </div>
